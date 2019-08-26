1246aafec5848625b673eab0ff18ed1abad3bd73
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-26 19:19:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class App < Sinatra::Base
       if @operation == "add"
         "#{@number1 + @number2}"
       elsif @operation == "subtract"
-        "#{@number1 + @number2}"
+        "#{@number2 + @number1}"
       elsif @operation == "divide"
         "#{@number1 / @number2}"
       elsif @operation == "multiply"
