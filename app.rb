0a1dc7bd215edc2fc10c5bade60fbd4d5025bfe3
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-26 19:06:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,6 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    str = ""
-    @number.times { str + "#{@phrase}\n" }
-    str
+    @number.times puts "#{@phrase}\n"
   end 
 end
